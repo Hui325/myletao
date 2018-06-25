b684c2e99e4787bf8e9c5bc064fd4f7949f086ae
fan <350813476@qq.com>
2018-06-25 21:29:33 +0800

    首页静态统计图完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -68,7 +68,7 @@ $(function () {
       data:$('#form').serialize(),
       dataType : 'json',
       success : function(info){
-        console.log(info);
+        //console.log(info);
         if(info.success){
           location.href="index.html";
         }
