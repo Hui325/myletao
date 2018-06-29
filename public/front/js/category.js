eb01e739de22db055ee2a0282e4b7d675f29ac33
fan <350813476@qq.com>
2018-06-29 08:30:12 +0800

    分类侧边栏渲染完成

--- public/front/js/category.js
+++ public/front/js/category.js
@@ -1,3 +1,16 @@
 /**
  * Created by HP on 2018/6/28.
  */
+$(function(){
+  //去后台获取数据，渲染数据到前台
+  $.ajax({
+    type:'get',
+    url:'/category/queryTopCategory',
+    dataType:'json',
+    success:function(info){
+      console.log(info);
+      var htmlStr = template('catetmp',info);
+      $('.lt_category_left ul').html(htmlStr);
+    }
+  })
+})
